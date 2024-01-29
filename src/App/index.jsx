9157300b25e176b914de98bd42f0fd29ec7b2bae
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-29 08:13:22 -0300

    Class 6 adding styles

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,6 +1,6 @@
-import './App.css';
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
+import './styles.css';
+import Navbar from "../components/Navbar"
+import Home from "../components/Home"
 
 function App() {
   return (
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,10 @@
+import "../../App/styles.css"
 import './styles.css';
 
 const Home = props => (
-  <>
+  <div className="content">
     <h1> Home Page </h1>
-  </>
+  </div>
 );
 
 export default Home;
