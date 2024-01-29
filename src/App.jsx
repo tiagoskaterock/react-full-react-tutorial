5f835baa0cc0e2b9bef01b2182cefcc62182ba12
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-29 07:20:14 -0300

    Class 5 multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css';
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
 
 function App() {
-
-  const title = "Welcome the our blog!!!"
-  const likes = 50
-  const person = {
-    name: 'Yoshi',
-    age: '33'
-  }
-  const link = 'https://www.google.com'
-
   return (
     <div className="App">
-      <div className="content">
-        <h1>{ title }</h1>
-        <h2>{ person.name }</h2>
-        <h3>{ person.age } years old</h3>
-        <p>Liked { likes } times</p>
-        <a href={ link }>Website</a>
-
-      </div>
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+const Home = props => (
+  <>
+    <h1> Home Page </h1>
+  </>
+);
+
+export default Home;
