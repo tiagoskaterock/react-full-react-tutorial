c5e4626e79a7997a76260662c0c9fc5a7610348e
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-29 06:50:33 -0300

    Class 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  const title = "Welcome the our blog!!!"
+  const likes = 50
+  const person = {
+    name: 'Yoshi',
+    age: '33'
+  }
+  const link = 'https://www.google.com'
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <h2>{ person.name }</h2>
+        <h3>{ person.age } years old</h3>
+        <p>Liked { likes } times</p>
+        <a href={ link }>Website</a>
+
       </div>
     </div>
   );
