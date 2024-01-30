42b8e8c5d80bc038133df47840b554d78ee6ceca
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-29 22:25:23 -0300

    Class 6 click events

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,28 @@
 import "../../App/styles.css"
 import './styles.css';
 
-const Home = props => (
-  <div className="content">
-    <h1> Home Page </h1>
-  </div>
-);
+function Home() {
+
+  function handleClick() {
+    alert('clicked')
+  } 
+
+  function handleClickAgain(name) {
+    alert('Hello ' + name + '!')
+  } 
+
+  return (
+    <div className="content">
+      <h1> Home Page </h1>
+      <button onClick={ handleClick }>
+        Click me
+      </button>
+
+      <button onClick={ () => { handleClickAgain('Tiago') } }>
+        Click me again
+      </button>
+    </div>
+  )
+}
 
 export default Home;
